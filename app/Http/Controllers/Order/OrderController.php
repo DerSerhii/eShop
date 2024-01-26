eb40fc907b62fc26b46af2e392335971a5f6de72
serhii <proektssd@gmail.com>
2024-01-26 13:25:19 +0200

    refactor(OrderController): added CustomerService to controller

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -4,19 +4,23 @@ namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Order\OrderRequest;
+use eShop\Infrastructure\Customer\Service\CustomerService;
 
 class OrderController extends Controller
 {
     public function store(
-        OrderRequest $request
+        OrderRequest $request,
+        CustomerService $customerService
     ): array
     {
         $email = $request->input('email');
         $productLine = $request->input('products', []);
 
+        $customer = $customerService->add($email);
+
         return [
             'data' => [
-                'email' => $email,
+                'email' => $customer->getEmail()->getValue(),
                 'products' => $productLine,
                 'total' => 0,
                 'bonus' => 0
