913632914be0a96e81919cf8a2325c97631d9edb
serhii <proektssd@gmail.com>
2024-01-26 22:21:52 +0200

    refactor(OrderController): added logic from ProductLineService

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -5,23 +5,57 @@ namespace App\Http\Controllers\Order;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Order\OrderRequest;
 use eShop\Infrastructure\Customer\Service\CustomerService;
+use eShop\Infrastructure\Services\ProductLineService;
+use Illuminate\Http\JsonResponse;
 
 class OrderController extends Controller
 {
     public function store(
-        OrderRequest $request,
-        CustomerService $customerService
-    ): array
+        OrderRequest                $request,
+        CustomerService             $customerService,
+        ProductLineService          $productLineService
+    ): array|JsonResponse
     {
         $email = $request->input('email');
-        $productLine = $request->input('products', []);
+        $products = $request->input('products', []);
+
+        $productLineService->setProducts($products);
+
+        $missingIds = $productLineService->validateIds();
+
+        if (!empty($missingIds)) {
+            $message = 'Product(s) with ID(s): ' .
+                implode(', ', $missingIds) . ' do not exist';
+            $errorData = [
+                'message' => $message
+            ];
+            return response()->json($errorData, 400);
+        }
+
+        $productWithInsufficientQuantity = $productLineService->validateQuantities();
+
+        if (!empty($productWithInsufficientQuantity)) {
+            $errorMessages = [];
+            foreach ($productWithInsufficientQuantity as $invalidProduct) {
+                $errorMessage = "Product with Id {$invalidProduct['id']} ordered in quantity " .
+                    "{$invalidProduct['quantityInProductLine']}," .
+                    " only {$invalidProduct['quantityInProductStorage']} in storage";
+                $errorMessages[] = $errorMessage;
+            }
+            $errorData = [
+                'message' => $errorMessages
+            ];
+            return response()->json($errorData, 400);
+        }
+
+        $productLine = $productLineService->getProductLine();
 
         $customer = $customerService->add($email);
 
         return [
             'data' => [
                 'email' => $customer->getEmail()->getValue(),
-                'products' => $productLine,
+                'products' => $products,
                 'total' => 0,
                 'bonus' => 0
             ]
