8765d3cf8efac4916d6b86c9c40adff4b6a0c942
serhii <proektssd@gmail.com>
2024-01-26 13:10:13 +0200

    refactor(OrderController): added OrderRequest

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Order\OrderRequest;
 
 class OrderController extends Controller
 {
-    public function store(): array
+    public function store(
+        OrderRequest $request
+    ): array
     {
+        $email = $request->input('email');
+        $productLine = $request->input('products', []);
+
         return [
             'data' => [
-                'email' => 'example@gmail.com',
-                'products' => [
-                    'productLine'
-                ],
+                'email' => $email,
+                'products' => $productLine,
                 'total' => 0,
                 'bonus' => 0
             ]
