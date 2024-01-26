0739ddc539d61011b9d300fb56ca84a4d0f03c7b
serhii <proektssd@gmail.com>
2024-01-26 12:24:18 +0200

    feat(Name): added common value object Name

---
+++ src/eShop/Domain/Common/ValueObject/Name.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace eShop\Domain\Common\ValueObject;
+
+use InvalidArgumentException;
+
+class Name
+{
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $this->validateName($name);
+    }
+
+    private function validateName($name)
+    {
+        if (empty($name)) {
+            throw new InvalidArgumentException('Name cannot be empty.');
+        }
+        return $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
