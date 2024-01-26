6e565fee90ab4e433c7dc52981ec40140f15f2d2
serhii <proektssd@gmail.com>
2024-01-26 21:48:16 +0200

    feat(CartProduct): added CartProduct entity

---
+++ src/eShop/Domain/Cart/Entity/CartProduct.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace eShop\Domain\Cart\Entity;
+
+
+use eShop\Domain\Common\ValueObject\Id;
+use eShop\Domain\Common\ValueObject\OrderQuantity;
+use eShop\Domain\Product\Entity\ProductStorage;
+
+final class CartProduct
+{
+    private Id $id;
+    private ProductStorage $product;
+    private readonly OrderQuantity $orderQuantity;
+
+    public function __construct(
+        ProductStorage $product,
+        OrderQuantity $orderQuantity)
+    {
+        $this->product = $product;
+        $this->orderQuantity = $orderQuantity;
+    }
+
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ProductStorage
+    {
+        return $this->product;
+    }
+
+    public function getOrderQuantity(): OrderQuantity
+    {
+        return $this->orderQuantity;
+    }
+
+    public function setId(Id $id): void
+    {
+        $this->id = $id;
+    }
+}
