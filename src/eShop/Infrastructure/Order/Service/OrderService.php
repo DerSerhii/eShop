fde4242a88d07b7927cc11ac4af9b52a53bb2a43
serhii <proektssd@gmail.com>
2024-01-27 14:59:10 +0200

    refactor(OrderService): added setting order total and bonus

--- src/eShop/Infrastructure/Order/Service/OrderService.php
+++ src/eShop/Infrastructure/Order/Service/OrderService.php
@@ -3,17 +3,20 @@
 namespace eShop\Infrastructure\Order\Service;
 
 use eShop\Domain\Cart\Entity\Cart;
-use eShop\Domain\Customer\Entity\Customer;
 use eShop\Domain\Order\Entity\Order;
 use eShop\Domain\Order\Service\OrderServiceInterface;
+use eShop\Domain\Order\ValueObject\OrderBonus;
+use eShop\Domain\Order\ValueObject\OrderTotal;
 use eShop\Infrastructure\Order\Repository\OrderRepository;
 use eShop\Infrastructure\Order\Repository\ProductOrderRepository;
+use eShop\Infrastructure\Services\OrderCalculatorService;
 
 class OrderService implements OrderServiceInterface
 {
     public function __construct(
         private readonly ProductOrderRepository $productOrderRepository,
-        private readonly OrderRepository $orderRepository
+        private readonly OrderRepository $orderRepository,
+        private readonly OrderCalculatorService $orderCalculatorService
     ) {
     }
 
@@ -38,6 +41,16 @@ class OrderService implements OrderServiceInterface
             $orderProductLine[] = $productOrder;
         }
 
-        return $this->orderRepository->create($customer, $orderProductLine);
+        $order = $this->orderRepository->create($customer, $orderProductLine);
+
+        $this->orderCalculatorService->setProductLine($orderProductLine);
+
+        $orderTotal = $this->orderCalculatorService->getOrderTotal();
+        $orderBonus = $this->orderCalculatorService->getOrderBonus();
+
+        $order->setOrderTotal(new OrderTotal($orderTotal));
+        $order->setOrderBonus(new OrderBonus($orderBonus));
+
+        return $order;
     }
 }
