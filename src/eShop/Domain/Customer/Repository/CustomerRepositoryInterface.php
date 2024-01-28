23b91e84db5afd4d0be52740a72b22be8502a5a5
serhii <proektssd@gmail.com>
2024-01-29 01:06:55 +0200

    refactor(OrderTransformer): unused classes removed

--- src/eShop/Domain/Customer/Repository/CustomerRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace eShop\Domain\Customer\Repository;
-
-interface CustomerRepositoryInterface
-{
-    public function create(string $email);
-}
--- src/eShop/Domain/Customer/Service/CustomerServiceInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace eShop\Domain\Customer\Service;
-
-interface CustomerServiceInterface
-{
-    public function add(string $email);
-}
