6fc6530d6faa52e02e356c9087f08b6ad931990f
serhii <proektssd@gmail.com>
2024-01-29 00:50:40 +0200

    refactor(config/shop): added discount

--- config/shop.php
+++ config/shop.php
@@ -2,15 +2,6 @@
 
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Bonus Threshold
-    |--------------------------------------------------------------------------
-    |
-    | This value is necessary to calculate the bonus in the order.
-    | Rule: If the order amount is greater than 'bonus_threshold' then the bonus
-    | is 5% of the difference between the amount and 'bonus_threshold'.
-    |
-    */
     'bonus_threshold' => 100,
+    'discount' => 5
 ];
