d943fed32baf83254e69550fc5da2d4343528a5a
serhii <proektssd@gmail.com>
2024-01-29 00:58:37 +0200

    refactor(OrderService): returns OrderProductCollection instead Order, ProductLine creation removed

--- src/eShop/Infrastructure/Order/Service/OrderService.php
+++ src/eShop/Infrastructure/Order/Service/OrderService.php
@@ -2,59 +2,45 @@
 
 namespace eShop\Infrastructure\Order\Service;
 
-use eShop\Domain\Cart\Entity\Cart;
-use eShop\Domain\Order\Entity\Order;
+use eShop\Domain\Cart\Entity\CartProductCollection;
+use eShop\Domain\Order\Entity\OrderProductCollection;
 use eShop\Domain\Order\Service\OrderServiceInterface;
+use eShop\Domain\Order\Settings\OrderSettings;
 use eShop\Domain\Order\ValueObject\OrderBonus;
 use eShop\Domain\Order\ValueObject\OrderTotal;
+use eShop\Infrastructure\Order\Repository\OrderProductCollectionRepository;
 use eShop\Infrastructure\Order\Repository\OrderRepository;
-use eShop\Infrastructure\Order\Repository\ProductOrderRepository;
-use eShop\Infrastructure\Services\BonusThresholdService;
-use eShop\Infrastructure\Services\OrderCalculatorService;
 
 class OrderService implements OrderServiceInterface
 {
     public function __construct(
-        private readonly ProductOrderRepository $productOrderRepository,
-        private readonly OrderRepository $orderRepository,
-        private readonly OrderCalculatorService $orderCalculatorService,
-        private readonly BonusThresholdService $bonusThresholdService
+        private readonly OrderRepository                  $orderRepository,
+        private readonly OrderCalculatorService           $orderCalculatorService,
+        private readonly OrderProductCollectionRepository $orderProductCollectionRepository,
+        private readonly OrderSettings                    $orderSettings
     ) {
     }
 
-    public function createOrder(Cart $cart): Order
+    public function makeOrder(CartProductCollection $cart): OrderProductCollection
     {
-        $customer = $cart->getCustomer();
-        $productLine = $cart->getProductLine();
-
-        $orderProductLine = [];
-
-        foreach ($productLine as $cartProduct) {
-            $productStorage = $cartProduct->getProduct();
-            $orderQuantity = $cartProduct->getOrderQuantity();
-            $productPrice = $productStorage->getPrice();
-
-            $productOrder = $this->productOrderRepository->create(
-                $productStorage,
-                $orderQuantity,
-                $productPrice
-            );
-
-            $orderProductLine[] = $productOrder;
-        }
-
-        $order = $this->orderRepository->create($customer, $orderProductLine);
-
-        $bonusThreshold = $this->bonusThresholdService->getBonusThreshold();
+        $bonusThreshold = $this->orderSettings->getBonusThreshold();
         $this->orderCalculatorService->setBonusThreshold($bonusThreshold);
-        $this->orderCalculatorService->setProductLine($orderProductLine);
+
+        $discount = $this->orderSettings->getDiscount();
+        $this->orderCalculatorService->setDiscount($discount);
+
+        $cartProducts = $cart->getCartProductCollection();
+        $this->orderCalculatorService->setProductList($cartProducts);
 
         $orderTotal = $this->orderCalculatorService->getOrderTotal();
         $orderBonus = $this->orderCalculatorService->getOrderBonus();
 
-        $order->setOrderTotal(new OrderTotal($orderTotal));
-        $order->setOrderBonus(new OrderBonus($orderBonus));
+        $order = $this->orderRepository->create(
+            $cart->getEmail(),
+            new OrderTotal($orderTotal),
+            new OrderBonus($orderBonus)
+        );
 
-        return $order;
+        return $this->orderProductCollectionRepository->create($order, $cartProducts);
     }
 }
