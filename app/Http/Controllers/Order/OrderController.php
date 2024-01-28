0ada102069166d3d9d31cfb47fcac3e2cdac8325
serhii <proektssd@gmail.com>
2024-01-29 01:05:06 +0200

    refactor(OrderTransformer): changed product validation, removed customer

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -3,76 +3,37 @@
 namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Order\OrderRequest;
+use App\Http\Requests\Order\OrderValidator;
 use eShop\Infrastructure\Cart\Service\CartService;
-use eShop\Infrastructure\Customer\Service\CustomerService;
 use eShop\Infrastructure\Order\Service\OrderService;
-use eShop\Infrastructure\Services\OrderTransformService;
-use eShop\Infrastructure\Services\ProductLineService;
+use eShop\Infrastructure\Order\Transformer\OrderTransformer;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function store(
-        OrderRequest          $request,
-        CustomerService       $customerService,
-        ProductLineService    $productLineService,
-        CartService           $cartService,
-        OrderService          $orderService,
-        OrderTransformService $orderTransformService
-    ): array|JsonResponse
+        Request          $request,
+        OrderValidator   $orderValidator,
+        CartService      $cartService,
+        OrderService     $orderService,
+        OrderTransformer $orderTransformer,
+
+    ): JsonResponse
     {
+        $validateData = $orderValidator->validate($request);
+
+        if (!empty($validateData)) {
+            return response()->json(['errors' => $validateData], 422);
+        }
+
         $email = $request->input('email');
         $products = $request->input('products', []);
 
-        $productLineService->setProducts($products);
+        $cart = $cartService->makeCart($email, $products);
 
-        $missingIds = $productLineService->validateIds();
+        $order = $orderService->makeOrder($cart);
 
-        if (!empty($missingIds)) {
-            $message = 'Product(s) with ID(s): ' .
-                implode(', ', $missingIds) . ' do not exist';
-            $errorData = [
-                'message' => $message
-            ];
-            return response()->json($errorData, 400);
-        }
-
-        $duplicateIds = $productLineService->validateDuplicateIds();
-
-        if(!empty($duplicateIds)) {
-            $message = 'The products has duplicates Ids: ' .
-                implode(', ', $duplicateIds) . '.';
-            $errorData = [
-                'message' => $message
-            ];
-            return response()->json($errorData, 400);
-        }
-
-        $productWithInsufficientQuantity = $productLineService->validateQuantities();
-
-        if (!empty($productWithInsufficientQuantity)) {
-            $errorMessages = [];
-            foreach ($productWithInsufficientQuantity as $invalidProduct) {
-                $errorMessage = "Product with Id {$invalidProduct['id']} ordered in quantity " .
-                    "{$invalidProduct['quantityInProductLine']}," .
-                    " only {$invalidProduct['quantityInProductStorage']} in storage";
-                $errorMessages[] = $errorMessage;
-            }
-            $errorData = [
-                'message' => $errorMessages
-            ];
-            return response()->json($errorData, 400);
-        }
-
-        $productLine = $productLineService->getProductLine();
-
-        $customer = $customerService->add($email);
-
-        $cart = $cartService->add($customer, $productLine);
-
-        $order = $orderService->createOrder($cart);
-
-        return $orderTransformService->toArray($order);
+        return $orderTransformer->transform($order);
     }
 }
