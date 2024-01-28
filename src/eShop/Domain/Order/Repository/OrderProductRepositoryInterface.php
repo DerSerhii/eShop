d4df8f60e13887bf750be4786f46cb556b337632
serhii <proektssd@gmail.com>
2024-01-29 00:41:00 +0200

    refactor(OrderProductRepositoryInterface): renamed from ProductOrderRepositoryInterface

--- src/eShop/Domain/Order/Repository/OrderProductRepositoryInterface.php
+++ src/eShop/Domain/Order/Repository/OrderProductRepositoryInterface.php
@@ -3,12 +3,14 @@
 namespace eShop\Domain\Order\Repository;
 
 use eShop\Domain\Common\ValueObject\OrderQuantity;
+use eShop\Domain\Order\Entity\Order;
 use eShop\Domain\Product\Entity\ProductStorage;
 use eShop\Domain\Product\ValueObject\Price;
 
-interface ProductOrderRepositoryInterface
+interface OrderProductRepositoryInterface
 {
     public function create(
+        Order $order,
         ProductStorage $product,
         OrderQuantity $orderQuantity,
         Price $productPrice
