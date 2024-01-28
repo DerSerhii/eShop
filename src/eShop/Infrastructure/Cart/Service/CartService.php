54bffed38f48a33a73273ace8569c12cc4c0b6a9
serhii <proektssd@gmail.com>
2024-01-29 00:35:02 +0200

    refactor(CartServiceInterface): returns CartProductCollection instead Cart

--- src/eShop/Infrastructure/Cart/Service/CartService.php
+++ src/eShop/Infrastructure/Cart/Service/CartService.php
@@ -3,19 +3,24 @@
 namespace eShop\Infrastructure\Cart\Service;
 
 
-use eShop\Domain\Cart\Entity\Cart;
+use eShop\Domain\Cart\Entity\CartProductCollection;
 use eShop\Domain\Cart\Service\CartServiceInterface;
-use eShop\Domain\Customer\Entity\Customer;
+use eShop\Domain\Common\ValueObject\Email;
+use eShop\Infrastructure\Cart\Repository\CartProductCollectionRepository;
 use eShop\Infrastructure\Cart\Repository\CartRepository;
 
 class CartService implements CartServiceInterface
 {
     public function __construct(
-        private readonly CartRepository $cartRepository
+        private readonly CartRepository $cartRepository,
+        private readonly CartProductCollectionRepository $cartProductCollectionRepository
     ) {
     }
-    public function add(Customer $customer, array $productLine): Cart
+    public function makeCart(string $email, array $products): CartProductCollection
     {
-        return $this->cartRepository->create($customer, $productLine);
+        $email = new Email($email);
+        $cart = $this->cartRepository->create($email);
+
+        return $this->cartProductCollectionRepository->create($cart, $products);
     }
 }
