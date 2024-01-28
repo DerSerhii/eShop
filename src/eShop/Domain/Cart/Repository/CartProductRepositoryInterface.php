0a626b7e69674c5bee833ba6f58177ae513c41d2
serhii <proektssd@gmail.com>
2024-01-29 00:29:38 +0200

    refactor(CartProductRepositoryInterface): added Cart parameter

--- src/eShop/Domain/Cart/Repository/CartProductRepositoryInterface.php
+++ src/eShop/Domain/Cart/Repository/CartProductRepositoryInterface.php
@@ -2,11 +2,13 @@
 
 namespace eShop\Domain\Cart\Repository;
 
+use eShop\Domain\Cart\Entity\Cart;
 use eShop\Domain\Product\Entity\ProductStorage;
 
 interface CartProductRepositoryInterface
 {
     public function create(
+        Cart $cart,
         ProductStorage $storageProduct,
         int $quantity
     );
