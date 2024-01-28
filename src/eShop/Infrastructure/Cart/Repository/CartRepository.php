689920dd4ff8fbe2d1835acfd6cad91ddb9f9203
serhii <proektssd@gmail.com>
2024-01-29 00:31:17 +0200

    refactor(CartRepository): customer replaced with email

--- src/eShop/Infrastructure/Cart/Repository/CartRepository.php
+++ src/eShop/Infrastructure/Cart/Repository/CartRepository.php
@@ -4,12 +4,12 @@ namespace eShop\Infrastructure\Cart\Repository;
 
 use eShop\Domain\Cart\Entity\Cart;
 use eShop\Domain\Cart\Repository\CartRepositoryInterface;
-use eShop\Domain\Customer\Entity\Customer;
+use eShop\Domain\Common\ValueObject\Email;
 
 class CartRepository implements CartRepositoryInterface
 {
-    public function create(Customer $customer, array $productLine): Cart
+    public function create(Email $email): Cart
     {
-        return new Cart($customer, $productLine);
+        return new Cart($email);
     }
 }
